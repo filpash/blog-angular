651a3366ae5054fdf7f85e845ac30d259c54187a
Filipp <pilip.pashkevich@gmail.com>
2020-10-24 17:10:57 +0300

    creating services for authorization

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,12 +1,14 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule} from '@angular/router';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
 import {AdminLayoutComponent} from './shared/components/admin-layout/admin-layout.component';
 import {LoginPageComponent} from './login-page/login-page.component';
 import {DashboardPageComponent} from './dashboard-page/dashboard-page.component';
 import {CreatePageComponent} from './create-page/create-page.component';
 import {EditPageComponent} from './edit-page/edit-page.component';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {AuthService} from './shared/services/auth.service';
 
 @NgModule({
   declarations: [
@@ -32,7 +34,8 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
       }
     ])
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthService]
 })
 export class AdminModule {
 
